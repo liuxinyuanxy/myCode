5f3a8cdbe8a5d0566707897f081cd340e5bcd650
liuxinyuanxy <liuxinyuanxy@163.com>
2021-11-19 15:02:54 +0800

    update

--- cfb.cpp
+++ cfb.cpp
@@ -10,17 +10,45 @@
 #define re register
 using namespace std;
 int n;
+int x[10005], y[10005];
+ll l[10005], r[10005];
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0), cout.tie(0);
-    int t;
-    cin >> t;
-    while (t--)
+    cin >> n;
+    int X = 0;
+    for (int i = 1; i <= n; ++i)
     {
-        int a, b, c;
-        cin >> a >> b >> c;
-        cout << ((a + c * 3) & 1) << endl;
+        cin >> x[i] >> y[i];
+        X += x[i];
     }
+    ll tot = 0, toty = 0x7fffffffffffffff;
+    int cnt = 0;
+    int avgx = X / n;
+    for (int i = 1; i <= n; ++i)
+        cnt += abs(avgx - x[i]);
+    tot = cnt;
+    cnt = 0;
+    ++avgx;
+    for (int i = 1; i <= n; ++i)
+        cnt += abs(avgx - x[i]);
+    if (cnt < tot)
+        tot = cnt;
+    sort(y + 1, y + 1 + n);
+    for (int i = 2; i <= n; ++i)
+    {
+        l[i] = l[i - 1] + (y[i] - y[i - 1] - 1) * 1ll * (i - 1);
+    }
+    for (int i = n - 1; i >= 1; --i)
+    {
+        r[i] = r[i + 1] + (y[i + 1] - y[i] - 1) * 1ll * (n - i);
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        cout << l[i] << " " << r[i] << endl;
+        toty = min(toty, l[i] + r[i]);
+    }
+    cout << tot + toty;
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,10 +1,28 @@
-#include <cstdio>
-#include <iostream>
-using namespace std;
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define read(x) scanf("%d", &x)
+
+int a[50][50];
+int b[50][50];
+int MyNegative(int x)
+{
+    if (x >= 0)
+    {
+        x = x ^ 10000000000000000000000000000000;
+        return x;
+    }
+    else
+    {
+        x = x & 0x7fffffff;
+        return x;
+    }
+}
 int main()
 {
-    char ch[5];
-    scanf("%s", ch);
-    cout << ch << endl;
+    int n;
+    read(n);
+    printf("%d", MyNegative(n));
     return 0;
 }
